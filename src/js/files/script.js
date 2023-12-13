ea3a553396172ad3e04e3dde291e6d96a1c162c5
Dima <uvarov3123@gmail.com>
2023-12-13 21:44:28 +0200

    add header

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -3,16 +3,45 @@ import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
 
-// window.addEventListener('load', pageLoad)
+window.addEventListener('load', pageLoad)
 
-// function pageLoad() {
-//     const htmlTag = document.documentElement
+function pageLoad() {
+    const htmlTag = document.documentElement
 
-//     document.addEventListener('click', e => {
-//         const targetElement = e.target
+    document.addEventListener('click', e => {
+        const targetElement = e.target
 
-//         if (targetElement.closest('.class')) {
-//             console.log('1');
-//         }
-//     })
-// }
+        if (targetElement.closest('.class')) {
+            console.log('1');
+        }
+
+        let catalog = document.querySelector('[data-catalog]');
+
+        if (catalog && targetElement.closest('[data-catalog-open]')) {
+            htmlTag.classList.toggle('open-catalog')
+        }
+        // || htmlTag.closest('.open-catalog') && !targetElement.closest(".catalog-header__body") && !targetElement.closest('[data-catalog-open]')
+        if (htmlTag.closest('.open-catalog') && !targetElement.closest(".catalog-header__body") && !targetElement.closest('[data-catalog-open]')) {
+            htmlTag.classList.toggle('open-catalog')
+        }
+
+        /////
+
+
+        if (targetElement.closest('[data-search]')) {
+            htmlTag.classList.toggle('open-search')
+
+            let input = document.querySelector('.search-header input')
+            input && input.focus()
+
+        }
+        if (htmlTag.closest('.open-search') && !targetElement.closest("[data-search]") && !targetElement.closest(".search-header")) {
+            htmlTag.classList.toggle('open-search')
+        }
+    })
+
+
+
+
+
+}
